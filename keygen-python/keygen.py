2f151055b41a9b15a806edce0f7bc9a3fcafc4e0
hutinel <alla.acosta@outlook.com>
2025-09-01 15:05:20 -0400

    ejemplo básico usando secrets

--- keygen-python/keygen.py
+++ keygen-python/keygen.py
@@ -4,3 +4,5 @@ import string
 caracteres = string.ascii_letters + string.digits + string.punctuation
 longitud = 16
 contraseña = ''.join(secrets.choice(caracteres) for i in range(longitud))
+
+print(f"Tu nueva contraseña es: {contraseña}")
