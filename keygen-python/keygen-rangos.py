48bf4d5afa1f2e3417a73edde913f842480f8e76
hutinel <alla.acosta@outlook.com>
2025-09-03 13:02:38 -0400

    se agrega libreria secrets y se genera un valor aleatorio para la letra que corta la fecha y la hora.

--- keygen-python/keygen-rangos.py
+++ keygen-python/keygen-rangos.py
@@ -1,10 +1,18 @@
 import datetime
+import secrets
+import string
+
 
 ahora = datetime.datetime.now()
 mes = ahora.strftime("%b")
 dia_numero = ahora.strftime("%d")
 ano_numero = ahora.strftime("%Y")
-
 hora = ahora.strftime("%H%M%S")
+letra_aleatoria = string.ascii_letters.lower()
+parte_aleatoria = ''.join(secrets.choice(letra_aleatoria) for _ in range(1))
 
-print("#" + mes.upper() + dia_numero + ano_numero + ".j" + hora + ".#")
+contraseña_patron = f"#{mes.upper()}{dia_numero}{ano_numero}{parte_aleatoria}{hora}.#"
+
+print(f"su nueva contraseña es: {contraseña_patron}")
+
+
