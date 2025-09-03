6ebb036e1da6a01fa30bb53ff17115cc8c18f2ae
hutinel <alla.acosta@outlook.com>
2025-09-03 12:33:38 -0400

    se agrega formato de hora

--- keygen-python/keygen-rangos.py
+++ keygen-python/keygen-rangos.py
@@ -4,4 +4,7 @@ ahora = datetime.datetime.now()
 mes = ahora.strftime("%b")
 dia_numero = ahora.strftime("%d")
 ano_numero = ahora.strftime("%Y")
-print("#" + mes.upper() + dia_numero + ano_numero + ".j")
+
+hora = ahora.strftime("%H%M%S")
+
+print("#" + mes.upper() + dia_numero + ano_numero + ".j" + hora + ".#")
